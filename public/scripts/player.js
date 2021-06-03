21b9286b2123d3610fb86a8eae023231ec08af63
Gabriel Ashkenazi <gabriel.ashkenazi1@gmail.com>
2021-06-03 13:16:49 -0400

    feat: clicking on one of the search results plays the song

--- public/scripts/player.js
+++ public/scripts/player.js
@@ -2,13 +2,13 @@ const socket = io();
 
 let isPlaying = false;
 
-document.getElementById('play-song').addEventListener("click", () => {
-  const pause = document.getElementById('pause-song');
-  const songId = document.getElementById('search-bar').value;
-  socket.emit('play', { resume: false, songId })
-  pause.innerHTML = '<i class="fas fa-pause" aria-hidden="true"></i>';
-  isPlaying = true;
-});
+// document.getElementById('play-song').addEventListener("click", () => {
+//   const pause = document.getElementById('pause-song');
+//   const songId = document.getElementById('search-bar').value;
+//   socket.emit('play', { resume: false, songId })
+//   pause.innerHTML = '<i class="fas fa-pause" aria-hidden="true"></i>';
+//   isPlaying = true;
+// });
 
 document.getElementById('pause-song').addEventListener("click", () => {
   const pause = document.getElementById('pause-song');
@@ -36,6 +36,7 @@ function getCookie(name) {
 
 document.getElementById('search-button').addEventListener("click", (e) => {
   e.preventDefault();
+  let ids = []
   const d = document.getElementById("search-results")
   d.innerHTML = "";
   fetch(`https://api.spotify.com/v1/search?q=${encodeURIComponent(document.getElementById('search-bar').value)}&type=track&market=US&limit=5`, {
@@ -44,13 +45,24 @@ document.getElementById('search-button').addEventListener("click", (e) => {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${getCookie("spotify_access")}`
     }
-  }).then(response => response.json())
-      .then((data) => {
+  }).then(response => response.json()).then((data) => {
         data.tracks.items.forEach((track) => {
+          ids.push(track.id)
           const explicit = track.explicit ? "[Explicit] " : "";
-          // console.log(`${track.name} ${explicit}by ${track.artists[0].name} | ${track.id}`)
+          // console.log(`${track.name} ${explicit} by ${track.artists[0].name} | ${track.id}`)
           d.innerHTML += `<li><a>${track.name} ${explicit} by ${track.artists[0].name} | ${track.id}</a></li>`
         });
+        const items = d.getElementsByTagName("li");
+        for (let i = 0; i < ids.length; i++) {
+          items[i].addEventListener("click", () => {
+            document.getElementById("search-bar").value = ids[i];
+            const pause = document.getElementById('pause-song');
+            const songId = document.getElementById('search-bar').value;
+            socket.emit('play', { resume: false, songId })
+            pause.innerHTML = '<i class="fas fa-pause" aria-hidden="true"></i>';
+            isPlaying = true;
+          })
+        }
       });
 })
 
